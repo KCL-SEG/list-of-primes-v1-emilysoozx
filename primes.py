8be19592281f2b91641acce7027ea550cc7d64ce
Emily <k21073231@kcl.ac.uk>
2023-10-02 12:56:15 +0100

    working solution

--- primes.py
+++ primes.py
@@ -3,23 +3,21 @@
 
 import math
 
-def is_prime(num): 
+def is_prime(num):
     if num < 2:
-        return False 
+        return False
     for i in range(2, int(math.sqrt(num) + 1)):
         if num % i == 0:
-            return False 
+            return False
     return True
 
 def primes(number_of_primes):
     prime_list = []
-    while len(list) < number_of_primes:
+    num = 2  # Initialize num to 2, the first prime number
+    while len(prime_list) < number_of_primes:
         if is_prime(num):
             prime_list.append(num)
         num += 1
     return prime_list
 
-# Test the function
-result = primes(10)
-print(result) 
-
+print(primes(100))
